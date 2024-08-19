320e6a23ff287bbefe308dfe46d31296c7f76e76
Kadisa-Cyber <kauan.dias.santos03@gmail.com>
2024-08-19 11:21:12 -0300

    By Kadisa

---
+++ MaiorValor.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ProjetoMaiorValorVetor
+{
+    partial class MaiorValor
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.listNumero = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnMostar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero.Location = new System.Drawing.Point(321, 96);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(147, 30);
+            this.txtNumero.TabIndex = 0;
+            // 
+            // listNumero
+            // 
+            this.listNumero.FormattingEnabled = true;
+            this.listNumero.Location = new System.Drawing.Point(274, 160);
+            this.listNumero.Name = "listNumero";
+            this.listNumero.Size = new System.Drawing.Size(265, 95);
+            this.listNumero.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Old English Text MT", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(474, 86);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(138, 52);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Adicionar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnMostar
+            // 
+            this.btnMostar.Font = new System.Drawing.Font("Old English Text MT", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostar.Location = new System.Drawing.Point(330, 286);
+            this.btnMostar.Name = "btnMostar";
+            this.btnMostar.Size = new System.Drawing.Size(138, 52);
+            this.btnMostar.TabIndex = 3;
+            this.btnMostar.Text = "Mostrar";
+            this.btnMostar.UseVisualStyleBackColor = true;
+            this.btnMostar.Click += new System.EventHandler(this.btnMostar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Old English Text MT", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(197, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Número:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Old English Text MT", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(231, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(325, 32);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Projeto maior valor no vetor";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMostar);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listNumero);
+            this.Controls.Add(this.txtNumero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.ListBox listNumero;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnMostar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MaiorValor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoMaiorValorVetor
+{
+    public partial class MaiorValor : Form
+    {
+        public MaiorValor()
+        {
+            InitializeComponent();
+        }
+
+        int[] numeros = new int[4];
+        int index = 0, aux = 0;
+
+        private void btnMostar_Click(object sender, EventArgs e)
+        {
+            if (index < 4)
+            {
+                MessageBox.Show("Digite todos os valores no vetor");
+            }
+            else
+            {
+                listNumero.Items.Clear();
+
+                for (int i = 0; i < numeros.Length; i++)
+                {
+                    if (numeros[i] > aux)
+                    {
+                        aux = numeros[i];
+                    }
+                }
+
+                listNumero.Items.Add(aux);
+            }
+            
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNumero.Text) == false)
+            {
+                if (index < numeros.Length)
+                {
+                    numeros[index] = int.Parse(txtNumero.Text);
+                    index++;
+
+                    if (index == numeros.Length)
+                    {
+                        btnAdd.Enabled = false;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Digite algum valor!");
+            }
+            txtNumero.Text = null;
+        }
+    }
+}
